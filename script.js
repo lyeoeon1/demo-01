a9e9172945f76e6744227e4faaf5e4cb956ebb91
TongThenHung <99159962+TongThenHung@users.noreply.github.com>
2022-10-01 03:21:02 +0700

    upload source code

---
+++ script.js
@@ -0,0 +1,20 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	const emailVal = email.value;
+
+	// check if it is a valid email
+	if (!validateEmail(emailVal)) {
+		form.classList.add('error');
+	} else {
+		form.classList.remove('error');
+		document.body.innerHTML = `<div style="display:flex; align-items: center; justify-content: center;"><h1 style="font-size: 4rem;text-transform: uppercase;font-weight: 400;letter-spacing: 5px;padding-top: 90px;"">Thank you!</h1></div>`;
+	}
+});
+
+function validateEmail(email) {
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+}
